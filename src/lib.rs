f384285b7bb063881db99ecb6d7b952c968ca385
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-02-02 11:14:32 +0900

    first render

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
+use web_sys;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn run() {
+    let document = web_sys::window().unwrap().document().unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // JavaScriptでは… const canvas = document.getElementById("myCanvas");
+    let canvas = document.get_element_by_id("myCanvas").unwrap();
+    let canvas = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    // JavaScriptでは… const ctx = canvas.getContext("2d");
+    let ctx = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    // JavaScriptでは…
+    // ctx.beginPath();
+    // ctx.rect(20, 40, 50, 50);
+    // ctx.fillStyle = "#FF0000";
+    // ctx.fill();
+    // ctx.closePath();
+    ctx.begin_path();
+    ctx.rect(20.0, 40.0, 50.0, 50.0);
+    ctx.set_fill_style(&JsValue::from_str("#FF0000"));
+    ctx.fill();
+    ctx.close_path();
 }
