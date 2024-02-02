d94a43f5883072ac0e3593942b0e79e8a8eb9ae2
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-02-02 12:46:38 +0900

    鬼が大豆にぶつかったら、その逆の方向に動くように

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,51 @@ impl Demon {
     }
 }
 
+struct Bean {
+    x: i32,
+    y: i32,
+    radius: i32,
+}
+
+impl Bean {
+    fn draw(&self, ctx: &web_sys::CanvasRenderingContext2d) {
+        ctx.begin_path();
+        let _ = ctx.arc(
+            self.x as f64,
+            self.y as f64,
+            self.radius as f64,
+            0.0,
+            std::f64::consts::PI * 2.0,
+        );
+        ctx.set_fill_style(&JsValue::from_str("#E1D3A9"));
+        ctx.fill();
+        ctx.close_path();
+    }
+
+    fn diameter(&self) -> i32 {
+        self.radius * 2
+    }
+}
+
+struct UserInput {
+    mouse_x: i32,
+    mouse_y: i32,
+}
+
+impl UserInput {
+    fn set_mouse_position(&mut self, x: i32, y: i32) {
+        self.mouse_x = x;
+        self.mouse_y = y;
+    }    
+}
+
 struct Game {
     canvas_context: web_sys::CanvasRenderingContext2d,
     canvas_width: i32,
     canvas_height: i32,
     demon: Demon,
+    bean: Bean,
+    user_input: UserInput,    
     game_loop_closure: Option<Closure<dyn FnMut()>>, // ゲームループクローザ
     game_loop_interval_handle: Option<i32>,          // ゲームループのハンドル
 }
@@ -64,11 +104,24 @@ impl Game {
             dy: dy,
         };
 
+        let bean = Bean {
+            x: canvas_width / 2,
+            y: canvas_height / 2,
+            radius: 10,
+        };
+
+        let user_input = UserInput {
+            mouse_x: 0,
+            mouse_y: 0,            
+        };
+
         Self {
             canvas_context,
             canvas_width,
             canvas_height,
             demon,
+            bean,
+            user_input,
             game_loop_closure: None,
             game_loop_interval_handle: None,
         }
@@ -87,6 +140,12 @@ impl Game {
         // 鬼の描画
         self.demon.draw(&self.canvas_context);
 
+        // 大豆の描画
+        self.bean.draw(&self.canvas_context);
+
+        // 衝突処理
+        self.collision_detection();           
+
         // 鬼の移動先
         let moved_demon_x = self.demon.x.saturating_add(self.demon.dx);
         let moved_demon_y = self.demon.y.saturating_add(self.demon.dy);
@@ -99,7 +158,26 @@ impl Game {
         // 鬼と上下の壁の衝突
         if moved_demon_y > self.canvas_height - self.demon.height || moved_demon_y < 0 {
             self.demon.dy = -self.demon.dy;
-        }        
+        }
+
+        // 大豆をマウスに追従させる
+        let canvas = self.canvas_context.canvas().unwrap();
+        let relative_x = self.user_input.mouse_x.saturating_sub(canvas.offset_left());
+        if relative_x > self.bean.diameter() && relative_x < self.canvas_width {
+            self.bean.x = relative_x.saturating_sub(self.bean.radius);
+        }
+        let relative_y = self.user_input.mouse_y.saturating_sub(canvas.offset_top());
+        if relative_y > self.bean.diameter() && relative_y < self.canvas_height {
+            self.bean.y = relative_y.saturating_sub(self.bean.radius);
+        }
+
+        // 鬼と大豆の衝突
+        if (self.bean.x - self.bean.radius < moved_demon_x && moved_demon_x < self.bean.x + self.bean.radius) ||
+           (self.bean.y - self.bean.radius < moved_demon_y && moved_demon_y < self.bean.y + self.bean.radius)
+        {
+            self.demon.dx = -self.demon.dx;
+            self.demon.dy = -self.demon.dy;
+        }
 
         // 鬼の移動
         self.demon.x = self.demon.x.saturating_add(self.demon.dx);
@@ -133,7 +211,27 @@ impl Game {
         let handle = window.request_animation_frame(closure.as_ref().unchecked_ref());
 
         self.game_loop_interval_handle = Some(handle.unwrap());
-    }      
+    }
+
+    // メソッドではなく、関連関数なので Game::set_input() として呼び出す
+    // 引数には自分自身を Rc<RefCell<>> で包んだものを渡す
+    pub fn set_input_event(game: Rc<RefCell<Self>>) {
+        let game_mouse_move = game.clone();
+        let document = web_sys::window().unwrap().document().unwrap();
+
+        let closure = Closure::new(Box::new(move |event: web_sys::MouseEvent| {
+            let mut g = game_mouse_move.borrow_mut();
+            g.user_input
+                .set_mouse_position(event.client_x(), event.client_y());
+        }) as Box<dyn FnMut(_)>);
+
+        document.set_onmousemove(Some(&closure.as_ref().unchecked_ref()));
+        // forget()するとRust側はdropされるが、into_js_value()されてブラウザ側に残る
+        closure.forget();         
+    }
+
+    fn collision_detection(&mut self) {
+    }    
 }
 
 fn random_integer(length: f64) -> i32 {
@@ -146,4 +244,5 @@ pub fn run() {
     let game = Game::new();
     let game = Rc::new(RefCell::new(game));
     Game::set_game_loop_and_start(game.clone());
+    Game::set_input_event(game.clone());    
 }
